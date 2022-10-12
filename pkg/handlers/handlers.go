ab80fb170544d9a73de559f97c52d38a54aa500d
mchabour <mourad.chabour@services.inter-invest.fr>
2022-10-12 21:42:32 +0200

    init project web

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"booking/pkg/config"
+	"booking/pkg/models"
+	"booking/pkg/render"
+	"net/http"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repositories
+func NewRepo(a *config.AppConfig) *Repository {
+
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, request *http.Request) {
+	remoteIp := request.RemoteAddr
+	m.App.Session.Put(request.Context(), "remote_ip", remoteIp)
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, request *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIp := m.App.Session.GetString(request.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIp
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{StringMap: stringMap})
+}
